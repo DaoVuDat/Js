b6939e31c42b4af67dd4f326f78dcfba8daf3ada
daovudat <dat.daovu@gmail.com>
2019-10-31 15:24:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/js/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.js;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+// Raw data https://jsonplaceholder.typicode.com/posts/
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
